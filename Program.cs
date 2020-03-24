b2260a2e8ae87511fa06fb8e9b67996cb9b9e747
Phillip Hogan <PhillipHogan1@outlook.com>
2020-03-23 20:48:45 -0700

    First Commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Whiteboarding_Practice___Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Reverse("How much wood could a woodchuck chuck, if a woodchuck could chuck wood?"));
+        }
+
+        public static string Reverse(string s)
+        {
+            if(s.Length == 0)
+            {
+                return "String is Empty";
+            }
+            else
+            {
+                string[] str = s.Split(" ");
+                
+                Stack<string> theStack = new Stack<string>();
+
+                string rev = "";
+                
+                for (int i = 0; i < str.Length; i++)
+                {
+                    theStack.Push(str[i]);
+                }
+
+                while (theStack.Count != 0)
+                {
+                    rev += " " + theStack.Pop();
+                }
+
+                return rev;
+            }
+        }
+    }
+}
